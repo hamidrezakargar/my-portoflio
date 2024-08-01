18dee15e47a8b7acaecda6e91aba7b02ee9dca17
hamidreza <hamidrezakargar44@gmail.com>
2024-08-01 16:46:42 +0330

    edit backgroundImage  :)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,43 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { TextPlugin } from "gsap/all";
 import { useRef } from "react";
+import imag1 from "../assets/images/image01.jpg";
+
 
 function Header() {
     const comp = useRef();
 
     useGSAP(() => {
+        gsap.registerPlugin(TextPlugin)
         const tl = gsap.timeline()
         tl.from("p", {
             duration: 1,
             opacity: 0,
             y: "+=200",
             stagger: 0.6
+        }).to("#text", {
+            text: "i am a web developer",
+            duration: 2.5,
+            ease: "bounce.out",
         })
     }, { scope: comp })
     return (
-        <div ref={comp} className="flex flex-col items-start gap-4" >
-            <p className="text-white text-4xl font-grotesk">
-                Hi there
-            </p>
+        <section className="bg-cover bg-gradient-to-t bg-clip-border h-screen w-screen " style={{ backgroundImage: "url(" + imag1 + ")",}} >
+            <div ref={comp} className="flex flex-col items-start gap-5 "  >
+                <p className="text-white text-4xl font-grotesk">
+                    Hi there
+                </p>
 
-            <p className="text-white text-8xl" >
-                Hi I'm <p id="myName" className="text-secondary font-semibold font-grotesk" >Hamidreza Kargar</p>
-            </p>
-            <p className="text-3xl" >
-                i am a Web developer
-            </p>
+                <p className="text-white text-8xl" >
+                    I am <p id="myName" className="text-secondary font-semibold font-grotesk" >Hamidreza Kargar</p>
+                </p>
 
-        </div>
+                <p id="text" className="text-3xl" >
+                </p>
+
+            </div>
+        </section>
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,13 @@ export default {
   ],
   theme: {
     extend: {
-      colors:{
-        primary:"#18315A",
-        secondary:"#FE3E57"
+      
+      colors: {
+        primary: "#18315A",
+        secondary: "#FE3E57"
       },
-      fontFamily:{
-        grotesk:["Space Grotesk" , "sans-serif"]
+      fontFamily: {
+        grotesk: ["Space Grotesk", "sans-serif"]
       }
     },
   },
